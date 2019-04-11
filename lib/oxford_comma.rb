4b0774f1c2e4c4b6b3a2f00f091ec63fe61de2eb
wei feng <theweifeng@gmail.com>
2019-04-11 23:17:58 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,13 @@
 def oxford_comma(array)
-
+  
+  string = array[0]
+  string = array.join(" and ") if array.length == 2
+  
+  if array.length >= 3
+    array[-1] = "and " + array.last
+    string = array.join(", ")
+  end
+  
+  string
+  
 end
