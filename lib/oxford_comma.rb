fea24ba67583a48cd7c5797e0d12cc59f326ffb6
wei feng <theweifeng@gmail.com>
2019-04-11 23:13:25 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -3,7 +3,7 @@ def oxford_comma(array)
   array.join(" and ") if array.length == 2
   
   if array.length >= 3
-    array.last = "and " + array.last
+    array[-1] = "and " + array.last
     array.join(", ")
   end
   
